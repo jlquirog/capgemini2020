5aad9edcafe2e5f7614cc845b1d3a3cf25974b7a
Jose Luis Quiroga <jl.quirog@ext.desigual.com>
2020-05-12 11:29:50 +0200

    Carga Json tratamiento datos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-class Album {
-    constructor(id, idImage,title,url,thumbnailUrl){
+class Photo {
+    constructor(albumId, id,title,url,thumbnailUrl){
+        this.albumId = albumId;
         this.id = id;
         this.tilte = titulo;
         this.url = url;
@@ -11,33 +12,69 @@ class Album {
 
 class UI {
 
-    loadJson (file) {
+    loadPhotos (file, typeView) {
     var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
+              alert('aqui esta llegando');
             var responseAlbum = JSON.parse(xhttp.responseText);
-            let albums = respuesta.libros;
+            let photos = responseAlbum;
   
-            var salida = '';
+            let salida = '';
             const ui = new UI();  
-    
+            console.log(photos);
+            console.log(photos[1]);
 
-            for(let item of albums) {
-                ui.readAlbum(item);
-            }
+
+            let albums = [];
+            let photos =[];
+            for(let item of photos) {
+                if (!album.includes(item.albumId)) {
+                    album.push(`Album id ${item.albumId}`);
+                }
+                photos.push(item);
+             }
+
+     
+             if (typeView = "list" ){
+                this.showList (albums);
+             } 
+             
+            
           
         }
       }
-       if (file === '') { 
-           file = 'https://jsonplaceholder.typicode.com/photos' 
-        }
-
+    
       xhttp.open("GET", file, true);
         
       xhttp.send();
     };
 
+    showList (albums) {
+
+    }
+
+    showGrid (photos) {
+
+    }
+
+    showDetail (photo) {
+
+    }
+
 
 }
 
-console.log('is working this?');
+
+const ui = new UI();
+
+//ui.loadPhotos('https://jsonplaceholder.typicode.com/photos');
+// *****   trabajando con una muestra en local por problmeas con la conexión con jsonplaceholder*****
+//Access to XMLHttpRequest at 'https://jsonplaceholder.typicode.com/photos' from origin 
+//'http://127.0.0.1:5500' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on //
+//the requested resource.
+
+ui.loadPhotos('/photos.json');
+
+//ui.loadAlbum('/photos.json');
+ 
